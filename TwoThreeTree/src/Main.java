7bc5e37ee0997d44bef399646c50bdb96857d755
fiddler <568200539@qq.com>
2020-01-09 11:31:59 +0800

    init

---
+++ TwoThreeTree/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        TwoThreeTree<Integer, Integer> twoThreeTree = new TwoThreeTree<>();
+        twoThreeTree.add(18, 1);
+        twoThreeTree.add(20, 1);
+        twoThreeTree.add(10, 1);
+        twoThreeTree.add(15, 1);
+        twoThreeTree.add(6, 1);
+        twoThreeTree.add(7, 1);
+        twoThreeTree.add(8, 1);
+        twoThreeTree.add(25, 1);
+        twoThreeTree.add(40, 1);
+        twoThreeTree.add(28, 1);
+        twoThreeTree.add(66, 1);
+        twoThreeTree.add(32, 1);
+        twoThreeTree.add(51, 1);
+        twoThreeTree.add(17, 1);
+        twoThreeTree.add(23, 1);
+        twoThreeTree.add(13, 1);
+        twoThreeTree.add(24, 1);
+    }
+}
